9dca88c325f2a8a623e264b0ba77b70fcc8283a2
onurhanaytac <onurhanaytac@gmail.com>
2017-02-21 20:57:10 +0300

    google maps added

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,8 @@
 
 		});
 
-	}()); 
+	}());
+
 
 	(function () {
 
@@ -241,3 +242,16 @@
 	
 
 });
+
+function initMap() {
+	var	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 39.895262, lng: 32.834343},
+		zoom: 16
+	});
+
+	var marker = new google.maps.Marker({
+		position: {lat: 39.895262, lng: 32.834343},
+		map: map,
+		title: 'OB Design - Lazer Kesim'
+    });
+}
